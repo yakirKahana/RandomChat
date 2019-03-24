1082170a1020a312562e171638d67477f51bfa6c
Yakir Kahana <Yakir.kah@gmail.com>
2019-03-24 16:57:37 +0200

    basic chat functionality

--- index.js
+++ index.js
@@ -6,13 +6,32 @@ const app = express();
 const server = http.createServer(app);
 const listen = io.listen(server);
 let users = [];
-let connection = [];
+let connections = [];
 
 
-
+// initialize server
 server.listen(3000);
 console.log('server online');
 
+
+// response to get requests
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/index.html');
+});
+
+// on io connect 
+listen.sockets.on('connection', socket => {
+    connections.push(socket);
+    console.log(`${connections.length} sockets connected`);
+
+    //disconnect
+    socket.on('disconnect', data => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log(`${connections.length} sockets connected`);
+    });
+
+    socket.on('send-msg', data => {
+        listen.sockets.emit('new-msg', { msg: data });
+    });
+
 });
