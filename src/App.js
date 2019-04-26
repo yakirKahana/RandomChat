2e75589d38c8c05c6c806cb9585db57e463fb8ec
Yakir Kahana <Yakir.kah@gmail.com>
2019-04-26 05:15:41 +0300

    import socket.io and e2ee

--- src/App.js
+++ src/App.js
@@ -3,27 +3,33 @@ import './App.css';
 
 import OutputBox from './components/output-box/output-box';
 import InputBox from './components/input-box/input-box';
+import io from 'socket.io-client';
+import e2ee from './e2ee';
+console.log(e2ee);
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
+    this.socket = io.connect();
+    this.e2e = new e2ee();
     this.state = {
-      messages:[{text:'שלום', me:true, key:Math.random()}]
+      messages: [{ text: 'שלום', me: true, key: Math.random() }]
     }
   }
 
-  handleMessageSent= (msgText)=>{
+  handleMessageSent = (msgText) => {
     let newMessages = this.state.messages;
-    newMessages.push({text:msgText,me:true,key:Math.random()});
-    this.setState({messages:newMessages});
+    newMessages.push({ text: msgText, me: true, key: Math.random() });
+    this.setState({ messages: newMessages });
   }
 
   render() {
-    setTimeout(()=>{
+    console.log(this.e2e);
+    setTimeout(() => {
       let newMessages = this.state.messages;
-      newMessages.push({text:'היי',me:false,key:Math.random()});
-      this.setState({messages:newMessages});
+      newMessages.push({ text: 'היי', me: false, key: Math.random() });
+      this.setState({ messages: newMessages });
 
-    },5000);
+    }, 5000);
     return (
       <div className="chat">
         <OutputBox messages={this.state.messages} />
